ea7eb41ee34d0dbf6cc27a980bdb27d347e7b820
bradish7y <bradish7y@gmail.com>
2014-11-16 15:18:16 +0800

    123

--- src/main/scala/org/xzm/test/TestTrait.scala
+++ src/main/scala/org/xzm/test/TestTrait.scala
@@ -1,7 +1,7 @@
 package org.xzm.test
 
 trait Logger {
-  def log(msg: String) {}
+  def log(msg: String)
   def info(msg: String) {
     log("info: " + msg)
   }
@@ -25,9 +25,34 @@ class LogTest extends Logger {
   }
 }
 
+trait Logged{
+  def log(msg: String){}
+}
+
+class BBC extends Logged{
+  def p(msg: String){
+    log(msg)
+  }
+}
+
+trait ConsoleLogger extends Logged{
+  val max: Int
+  override def log(msg: String){
+    println(msg+":"+max)
+  }
+}
+
+case class Currency(value: Double, unit: String)
+
+object Number{
+  def unapply(num: String):Option[Int]={
+    Some(Integer.parseInt(num))
+  }
+}
+
 object TestTrait {
   def main(args: Array[String]) {
-    val a = new LogTest
-    a.withdraw("123")
+    val Number(n) = "99"
+    println(n)
   }
 }
